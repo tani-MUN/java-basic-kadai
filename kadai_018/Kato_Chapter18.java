c66463daab285e81984e1d126a2174b2371c043a
tani <yuta_tani@icloud.com>
2025-04-17 02:31:46 +0900

    kadai_018を追加

---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,22 @@
+package text.kadai_018;
+
+abstract public class Kato_Chapter18 {
+	public String familyName;
+//	姓を表す
+	public String givenName;
+//	名を表す
+	public String address;
+//	住所を表す
+	
+	public void commonIntroduce() {
+		System.out.println("名前は加藤"+this.givenName+"です");
+		System.out.println("住所は東京都中野区◯×です");
+	}
+	abstract public void eachIntroduce();
+	
+	public void execIntroduce() {
+		commonIntroduce();
+		eachIntroduce();
+		System.out.println("");
+	}
+}
